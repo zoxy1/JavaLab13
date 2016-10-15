10239c8ae0d2f990dd42fb7588a6bd0cb3b3da5a
zoxy1 <zoxy1@yandex.ru>
2016-10-15 22:08:39 +0700

    add class Functions and file in.txt

---
+++ src/lab13/Functions.java
@@ -0,0 +1,17 @@
+package lab13;
+
+public class Functions {
+
+    public Functions() {
+	// TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+Calculation calculate=new Calculation(2);
+System.out.println(calculate.calculateFunction(1, 2, 3, 4, 2));
+    
+    
+    }
+
+}
