705e27563ac03039fcd8d296b4a0d64431447845
Andrey <zoxy1@yandex.ru>
2016-10-17 00:40:23 +0700

    made output in the console function value

--- src/lab13/Calculation.java
+++ src/lab13/Calculation.java
@@ -1,138 +1,152 @@
 package lab13;
+
 /**
- * This class describes the calculation of several functions. 
- * The function can be selected when creating a class object, 
- *  putting into the constructor function number. You see formula in lab #13.
+ * This class describes the calculation of several functions. The function can
+ * be selected when creating a class object, putting into the constructor
+ * function number. You see formula in lab #13.
+ * 
  * @author Andrey Kudryavtsev
- *
+ * 
  */
 public class Calculation {
-    private double a;
-    private double b;
-    private double c;
-    private double d;
-    private double x;
-    private int numberEquation = 1;
-    /**
-     *  The function can be selected when creating a class object, 
-     *  putting into the constructor function number. 
-     *  You see formula in lab #13.
-     * @param numberEquation - function number
-     *
-     */
-    
-    public Calculation(int numberEquation) {
-	this.setNumberEquation(numberEquation);
-    }
+	private double a;
+	private double b;
+	private double c;
+	private double d;
+	private double x;
+	private int numberEquation = 1;
 
-    private double lg(double a, double b) {
-	return Math.log(b) / Math.log(b);
-    }
+	/**
+	 * The function can be selected when creating a class object, putting into
+	 * the constructor function number. You see formula in lab #13.
+	 * 
+	 * @param numberEquation
+	 *            - function number
+	 * 
+	 */
 
-    private double ln(double a) {
-	return Math.log10(a) / Math.log10(Math.E);
-    }
+	public Calculation(int numberEquation) {
+		this.setNumberEquation(numberEquation);
+	}
 
-    private double ctg(double a) {
-	return 1.0 / Math.tan(a);
-    }
-/**
- * This method calculates the value of a function depending on the coefficients a, b, c, d
- * @param a - function coefficient;
- * @param b - function coefficient;
- * @param c - function coefficient;
- * @param d - function coefficient;
- * @param x - function parameter;
- * @return Return value have type double
- */
-    public double calculateFunction(double a, double b, double c, double d,
-	    double x) {
-	this.setA(a);
-	this.setB(b);
-	this.setC(c);
-	this.setD(d);
-	this.setX(x);
-	switch (this.numberEquation) {
-	case 1:
-	    return this.a * Math.pow(this.x, 2) + this.b * this.x + this.c
-		    * Math.pow(this.x, -2) + this.d;
+	private double lg(double a, double b) {
+		return Math.log(b) / Math.log(b);
+	}
 
-	case 2:
-	    return (double) Math.acos(this.a * Math.pow(this.x, 2)
-		    + Math.sqrt(this.b * Math.pow(this.x, 3)) );
-	case 3:
-	    return this.a * Math.pow(this.x, 4)
-		    - Math.pow(Math.sin(this.c * this.x), -0.5)
-		    + Math.pow(Math.cos(this.d * this.x), 2.1) + b;
-	case 4:
-	    return (Math.pow(
-		    (Math.cos(this.a * Math.pow(x, 2) + this.b * this.x)), 3) + this.c)
-		    / this.d + +lg(6, this.d);
-	case 5:
-	    return this.a * Math.sin(Math.pow(this.x, 2)) + this.b * this.x
-		    + this.c * Math.cos(Math.pow(this.x, -2)) + ln(this.d);
-	case 6:
-	    return ctg(this.a * Math.pow(this.x, 2)
-		    + ln(this.b * Math.pow(this.x, 3)) + this.c * this.x - 1.0
-		    / this.d);
+	private double ln(double a) {
+		return Math.log10(a) / Math.log10(Math.E);
+	}
 
-	default:
-	    return 0;
+	private double ctg(double a) {
+		return 1.0 / Math.tan(a);
+	}
+
+	/**
+	 * This method calculates the value of a function depending on the
+	 * coefficients a, b, c, d
+	 * 
+	 * @param a
+	 *            - function coefficient;
+	 * @param b
+	 *            - function coefficient;
+	 * @param c
+	 *            - function coefficient;
+	 * @param d
+	 *            - function coefficient;
+	 * @param x
+	 *            - function parameter;
+	 * @return Return value have type double
+	 */
+	public double calculateFunction(double a, double b, double c, double d,
+			double x) {
+		this.setA(a);
+		this.setB(b);
+		this.setC(c);
+		this.setD(d);
+		this.setX(x);
+		switch (this.numberEquation) {
+		case 1:
+			return this.a * Math.pow(this.x, 2) + this.b * this.x + this.c
+					* Math.pow(this.x, -2) + this.d;
+
+		case 2:
+			return (double) Math.acos(this.a * Math.pow(this.x, 2)
+					+ Math.sqrt(this.b * Math.pow(this.x, 3)));
+		case 3:
+			return this.a * Math.pow(this.x, 4)
+					- Math.pow(Math.sin(this.c * this.x), -0.5)
+					+ Math.pow(Math.cos(this.d * this.x), 2.1) + b;
+		case 4:
+			return (Math.pow(
+					(Math.cos(this.a * Math.pow(x, 2) + this.b * this.x)), 3) + this.c)
+					/ this.d + +lg(6, this.d);
+		case 5:
+			return this.a * Math.sin(Math.pow(this.x, 2)) + this.b * this.x
+					+ this.c * Math.cos(Math.pow(this.x, -2)) + ln(this.d);
+		case 6:
+			return ctg(this.a * Math.pow(this.x, 2)
+					+ ln(this.b * Math.pow(this.x, 3)) + this.c * this.x - 1.0
+					/ this.d);
+
+		default:
+
+			return 0;
+
+		}
 
 	}
 
-    }
-
-    public double getA() {
-	return a;
-    }
-
-    public void setA(double a) {
-	this.a = a;
-    }
-
-    public double getB() {
-	return b;
-    }
-
-    public void setB(double b) {
-	this.b = b;
-    }
-
-    public double getC() {
-	return c;
-    }
-
-    public void setC(double c) {
-	this.c = c;
-    }
-
-    public double getD() {
-	return d;
-    }
-
-    public void setD(double d) {
-	this.d = d;
-    }
-
-    public double getX() {
-	return x;
-    }
-
-    public void setX(double x) {
-	this.x = x;
-    }
-
-    public int getNumberEquation() {
-	return numberEquation;
-    }
-
-    public void setNumberEquation(int numberEquation) {
-	if (numberEquation >= 1 && numberEquation <= 20) {
-	    this.numberEquation = numberEquation;
-	} else {
-	    System.out.println("Номер уравнения должен быть от 1 до 20");
+	public double getA() {
+		return a;
 	}
-    }
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+	public double getD() {
+		return d;
+	}
+
+	public void setD(double d) {
+		this.d = d;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public int getNumberEquation() {
+		return numberEquation;
+	}
+
+	public void setNumberEquation(int numberEquation) {
+		if (numberEquation >= 1 && numberEquation <= 20) {
+			this.numberEquation = numberEquation;
+		} else {
+			System.out
+					.println("Номер уравнения должен быть от 1 до 20, по умолчанию выводится значения уравнения №1");
+		}
+	}
 
 }
--- src/lab13/Functions.java
+++ src/lab13/Functions.java
@@ -1,6 +1,7 @@
 package lab13;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,27 +21,26 @@ public class Functions {
 	public static void main(String[] args) throws IOException {
 
 		ArrayList<String> stringList = new ArrayList<>();
+		String file = "D:/_JAVA_Tusur/Workspace/JavaLab13.git/src/lab13/in.txt";
 		int numberLines = 0;
-		try (BufferedReader reader = new BufferedReader(
-				new InputStreamReader(
-						new FileInputStream(
-								"D:/_JAVA_Tusur/Workspace/JavaLab13.git/src/lab13/in.txt"),
-						StandardCharsets.UTF_8))) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(file), StandardCharsets.UTF_8))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				// System.out.println(line);
 
-				numberLines++;
+				numberLines++; //
 				stringList.add(line);
 
 			}
 			// System.out.println(numberLines);
 			reader.close();
 		} catch (IOException e) {
-			System.out.println("Ошибка ввода вывода");
+			System.out.println(e.getMessage());
 		}
-		ArrayList<Double> doubleList = new ArrayList<>();
+
 		Calculation variableCalculation = new Calculation(1);
+		ArrayList<Double> doubleList = new ArrayList<>();
 		for (int i = 0; i < numberLines; i++) {
 
 			String[] isParts = stringList.get(i).split(" ");
@@ -49,12 +49,21 @@ public class Functions {
 				doubleList.add(j, Double.parseDouble(isParts[j]));
 
 			}
+			Double x = doubleList.get(4);
+			Double y = 0.0d;
 
-			System.out.println(variableCalculation.calculateFunction(
-					doubleList.get(0), doubleList.get(1), doubleList.get(2),
-					doubleList.get(3), 10));
-			System.out.println("\n");
+			while (x <= doubleList.get(5)) {
 
+				y = variableCalculation.calculateFunction(doubleList.get(0),
+						doubleList.get(1), doubleList.get(2),
+						doubleList.get(3), x);
+				System.out.printf("%1$.3f", y);
+				System.out.print(" ");
+
+				x = x + doubleList.get(6);
+
+			}
+			System.out.print("\n");
 		}
 
 	}
